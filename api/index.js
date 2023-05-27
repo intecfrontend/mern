3f4b204a6b7fe9c94f2e4f9b8d395a6c1b5882a9
intecfrontend <53944321+intecfrontend@users.noreply.github.com>
2023-05-27 22:55:55 +0200

    r

--- api/index.js
+++ api/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
 app.use(cors());
-app.post('/register', (req, res) => {
-  res.json('info');
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://blog:0pSHR5aj32LLeYgI@cluster0.lcdamkf.mongodb.net/?retryWrites=true&w=majority');
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+
+  const userDoc = await User.create({
+    username, password
+  });
+
+  res.json(userDoc);
 });
 
 app.listen(4010);
+
+// 0pSHR5aj32LLeYgI
+// mongodb+srv://blog:0pSHR5aj32LLeYgI@cluster0.lcdamkf.mongodb.net/?retryWrites=true&w=majority
